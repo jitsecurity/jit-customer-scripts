2a8564b39b5ed3a60090402dc38f1ef1f6b6cc4f
jonathan <jonathan.rosenboim@jit.io>
2024-05-29 13:43:09 +0300

    Support env var

--- src/utils/github_topics_to_json_file.py
+++ src/utils/github_topics_to_json_file.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 from dotenv import load_dotenv
 from loguru import logger
@@ -6,11 +7,14 @@ from loguru import logger
 from src.shared.clients.github import get_teams_from_github_topics
 
 # Load environment variables from .env file.
+
+TEAMS_JSON_FILE = os.getenv("TEAMS_JSON_FILE", "../teams.json")
+
 load_dotenv()
 logger.remove()  # Remove default handler
 logger_format = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <2}</level> | {message}"
 logger.add(sys.stderr, format=logger_format)
 if __name__ == '__main__':
     teams = get_teams_from_github_topics()
-    with open("teams.json", "w") as file:
+    with open(TEAMS_JSON_FILE, "w") as file:
         file.write(teams.model_dump_json(indent=2))
