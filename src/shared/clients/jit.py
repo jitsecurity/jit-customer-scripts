872e05575108a59523f35b938babc66bb8c759f7
jonathan <jonathan.rosenboim@jit.io>
2024-08-19 18:41:42 +0300

    Edit getting existing teams

--- src/shared/clients/jit.py
+++ src/shared/clients/jit.py
@@ -61,7 +61,7 @@ def get_existing_teams(token: str) -> List[TeamAttributes]:
         return after
 
     try:
-        url = f"{get_jit_endpoint_base_url()}/teams?limit=30"
+        url = f"{get_jit_endpoint_base_url()}/teams?limit=50"
         headers = get_request_headers(token)
         existing_teams = []
         while True:
@@ -71,13 +71,13 @@ def get_existing_teams(token: str) -> List[TeamAttributes]:
                 after = _handle_response(response, existing_teams)
                 if not after:
                     break  # Exit loop if there's no 'after' for the next page
-                url = f"{get_jit_endpoint_base_url()}/teams?limit=30&after={after}"
+                url = f"{get_jit_endpoint_base_url()}/teams?limit=50&after={after}"
             else:
                 logger.error(
                     f"Failed to retrieve teams. Status code: {response.status_code}, {response.text}")
                 return []
 
-        logger.info("Retrieved existing teams successfully.")
+        logger.info(f"Retrieved existing teams successfully.")
         return [TeamAttributes(**team) for team in existing_teams]
     except Exception as e:
         logger.error(f"Failed to retrieve teams: {str(e)}")
