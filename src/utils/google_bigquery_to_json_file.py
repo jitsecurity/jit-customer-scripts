02da80aa27fa275f0c9f1e6b671e6b0e917a5650
jonathan <jonathan.rosenboim@jit.io>
2024-08-19 10:43:32 +0300

    Edit if name == main

--- src/utils/google_bigquery_to_json_file.py
+++ src/utils/google_bigquery_to_json_file.py
@@ -10,7 +10,13 @@ load_dotenv()
 logger.remove()  # Remove default handler
 logger_format = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <2}</level> | {message}"
 logger.add(sys.stderr, format=logger_format)
-if __name__ == '__main__':
+
+
+def generate_teams_json_file_from_bigquery():
     teams = get_teams_from_bigquery_view()
     with open("teams.json", "w") as file:
         file.write(teams.model_dump_json(indent=2))
+
+
+if __name__ == '__main__':
+    generate_teams_json_file_from_bigquery()
